9fae92dc202fe7b00f9e9a01460c84b3251f40ca
Bogdan Voicu <bogdanvoicu@Bogdans-MacBook-Pro.local>
2020-12-25 22:05:35 +0200

    Nou aplictie pentru avocat si register form cu toate campurile

--- LEXBOX/urls.py
+++ LEXBOX/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('home',include('LEXBOXapp.urls')),
     path('', include('LEXBOXapp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('LexBoxAvocat.urls')),
 ]
 if settings.DEBUG: 
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
