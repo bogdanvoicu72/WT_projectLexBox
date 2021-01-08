6195a429e28fdbb6b901eb0cd14dbd8bb6b5223a
Cristeee <tomoiucris787@gmail.com>
2021-01-08 10:55:29 +0200

    Link django to api - backend

--- api/controllers/generation/generate_service.py
+++ api/controllers/generation/generate_service.py
@@ -11,29 +11,25 @@ from common.response_builder import ResponseBuilder
 
 def build_document_input(user, request_json):
     return {
-        "law": request_json.get('law', None),
-        "first_name": user['firstName'],
-        "last_name": user['lastName'],
-        "address": user['address'],
-        "ci": user['ci'],
-        "assisted": request_json.get('assisted', None),
-        "accused": request_json.get('accused', None),
+        "law": str(request_json.get('articolul', None)) + "/" + str(request_json.get('alineatul', None)),
+        "first_name": request_json.get('nume', None),
+        "last_name": request_json.get('prenume', None),
+        "address": request_json.get('adresa_procedurala', None),
+        "assisted": request_json.get('asistat_avocat', None),
         "record": request_json.get('record', None),
-        "fine": request_json.get('fine', None),
-        "sanction": request_json.get('sanction', None),
-        "agreed_to_sanction": request_json.get('agreed_to_sanction', None),
-        "witnesses": request_json.get('witnesses', None)
+        "fine": request_json.get('amenda_platita', None),
+        "sanction": "",
+        "agreed_to_sanction": request_json.get('dorinta_amenda', None),
     }
 
 
 def generate_service(request_json, users, mail, minio_client, owner_email):
     try:
         doc = DocxTemplate("templates/request.docx")
-        user = users.find_one({"email": get_jwt_identity()})
-        context = build_document_input(user, request_json)
+        context = build_document_input(None, request_json)
         doc.render(context)
         doc.save("result.docx")
-        file_name = f'Cerere-{user["firstName"]}-{user["lastName"]}-{str(uuid.uuid1())}.docx'
+        file_name = 'asd.docx'
         with open("result.docx", "rb") as f:
             minio_client.upload_fileobj(f, 'lexbox', file_name)
         os.remove('result.docx')
@@ -41,10 +37,11 @@ def generate_service(request_json, users, mail, minio_client, owner_email):
 
         msg = Message('Notificare LexBox', sender=os.getenv('EMAIL'), recipients=[owner_email])
         msg.html = render_template("NotificationEmail.html",
-                                   firstName=user['firstName'],
-                                   lastName=user['lastName'],
+                                   firstName="user['firstName']",
+                                   lastName="user['lastName']",
                                    documentUrl=download_link)
         mail.send(msg)
         return ResponseBuilder.success({"download_link": download_link})
     except Exception as e:
+        print(e)
         return ResponseBuilder.failure(str(e))
