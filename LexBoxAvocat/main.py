a91a3c813109ec4ffe2ce312dcaf2d8aa81bca47
Ihurloi <ihurloi@gmail.com>
2021-01-08 20:20:31 +0200

    finish login page

--- LexBoxAvocat/main.py
+++ LexBoxAvocat/main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        return redirect('/')
+
 @app.route('/')
 def home():
-    return 'hello'
+    return 'home'
+
 
 if __name__ == '__main__':
     app.run(port=5002)
