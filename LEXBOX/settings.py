312e2c4008cab6629e17e7bf127594f08412e239
Bogdan Voicu <bogdanvoicu@Bogdans-MacBook-Pro.local>
2021-02-11 18:46:02 +0200

    add ../

--- LEXBOX/settings.py
+++ LEXBOX/settings.py
@@ -137,13 +137,14 @@ STATIC_URL = '/static/'
 STATIC_ROOT = "home.html/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'LEXBOX/static'),
+    os.path.join(BASE_DIR, '../LEXBOX/static'),
     #bootstrap
-    os.path.join(BASE_DIR, 'LEXBOX/static', 'home.html', 'boot'),
+    os.path.join(BASE_DIR, '../LEXBOX/static', 'home.html', 'boot'),
 
     # Document template goes here
-    os.path.join(BASE_DIR, 'LEXBOX/static', 'home.html', 'DocumentTemplate')
+    os.path.join(BASE_DIR, '../LEXBOX/static', 'home.html', 'DocumentTemplate')
 ]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
