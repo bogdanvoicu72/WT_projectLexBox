c58306e8ad6faf90c13aa1e3691797ec24161074
Bogdan Voicu <bogdanvoicu@Bogdans-MacBook-Pro.local>
2021-02-11 20:14:33 +0200

    ......

--- LEXBOX/wsgi.py
+++ LEXBOX/wsgi.py
@@ -11,6 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LEXBOX.settings')
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LEXBOX.settings')
+
+
+
