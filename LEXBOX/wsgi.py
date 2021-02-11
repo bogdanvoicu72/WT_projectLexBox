8034496bf6ff73d94ae9b8f14073b5e633a76071
Bogdan Voicu <bogdanvoicu@Bogdans-MacBook-Pro.local>
2021-02-11 20:18:08 +0200

    .......

--- LEXBOX/wsgi.py
+++ LEXBOX/wsgi.py
@@ -15,8 +15,8 @@ from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+application = WhiteNoise(application, root='/LEXBOXapp/static/files')
+application.add_files('/LEXBOXapp/static/files', prefix='more-files/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LEXBOX.settings')
 
