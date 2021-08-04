001d85d4c471fcaf0a2dc948ca3a3b3ac08f52ed
Bogdan Voicu <bogdanvoicu@Bogdans-MacBook-Pro.local>
2021-08-04 16:57:18 +0300

    test app

--- LEXBOX/settings.py
+++ LEXBOX/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MIDDLEWARE_CLASSES = (
@@ -133,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_URL = '/static/'
 
